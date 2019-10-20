b65a0950823679937011a7237a4c0fba3c0c5fbe
Eli Lindahl <linelij@dunwoody.edu>
2019-10-20 08:15:01 -0500

    Update to Master Branch

--- Form1.cs
+++ Form1.cs
@@ -22,7 +22,7 @@ namespace CatalogApp
         }
 
         //Database Connection String to be accessed  whenever manipulating the DB
-        public static string connectionString = "server=10.3.129.141;database=catalog;user id=lindahlish;password=Password01;";
+        public static string connectionString = "server=192.168.183.11;database=catalog;user id=lindahlish;password=Password01;";
         public static MySqlConnection conn = new MySqlConnection(connectionString);
         public static MySqlDataReader reader;
 
@@ -99,6 +99,102 @@ namespace CatalogApp
 
         }
 
+        // Method to perform a query on the DB and return data to the form
+        public static bool QueryDB(string searchData, bool titleBut, bool genreBut, ListView listView1)
+        {
+            List<Movie> movieList = new List<Movie>();
+            Movie movie;
+            string query;
+            MySqlCommand command;
+
+            //Check if the user entered anything into the search box
+            if (!String.IsNullOrEmpty(searchData))
+            {
+                //Search by movie title
+                if (titleBut == true)
+                {
+                    query = $"SELECT * FROM movies WHERE title LIKE '%{searchData}%' ORDER BY title ASC;";
+                    command = new MySqlCommand(query, conn);
+
+                    using (command)
+                    {
+                        conn.Open();
+                        reader = command.ExecuteReader();
+
+                        while (reader.Read())
+                        {
+                            movie = new Movie();
+                            movie.movieID = reader.GetInt32(0);
+                            movie.title = reader.GetString(1);
+                            movie.genre = reader.GetString(2);
+                            movie.year = reader.GetInt32(3);
+                            movie.rating = reader.GetString(4);
+                            movieList.Add(movie);
+                        }
+
+                        reader.Close();
+                        conn.Close();
+                    }
+
+                }
+
+                else if (genreBut == true)
+                {
+                    query = $"SELECT * FROM movies WHERE genre LIKE '%{searchData}%' ORDER BY title ASC;";
+                    command = new MySqlCommand(query, conn);
+
+                    using (command)
+                    {
+                        conn.Open();
+                        reader = command.ExecuteReader();
+
+                        while (reader.Read())
+                        {
+                            movie = new Movie();
+                            movie.movieID = reader.GetInt32(0);
+                            movie.title = reader.GetString(1);
+                            movie.genre = reader.GetString(2);
+                            movie.year = reader.GetInt32(3);
+                            movie.rating = reader.GetString(4);
+                            movieList.Add(movie);
+                        }
+
+                        reader.Close();
+                        conn.Close();
+                    }
+
+                }
+
+                else
+                {
+                    return false;
+                }
+
+                //Add the new movieList to the listView
+                int size = movieList.Count;
+
+                foreach (var item in movieList)
+                {
+                    string[] row = { Convert.ToString(item.movieID), item.title, item.genre, Convert.ToString(item.year), item.rating };
+                    var listItem = new ListViewItem(row);
+                    int compare = String.Compare(listItem.SubItems[0].Text, Convert.ToString(item.movieID));
+
+                    if (listView1.Items.Count != size)
+                    {
+                        listView1.Items.Add(listItem);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                return true;
+            }
+            //return false if search box text is null or both radio buttons are null
+            return false;
+        }
+
         private void entryBtn_Click(object sender, EventArgs e)
         {
             NewEntry entryForm = new NewEntry();
@@ -133,7 +229,14 @@ namespace CatalogApp
 
         private void searchBtn_Click(object sender, EventArgs e)
         {
+            bool unitTest1;
+            bool titleBut = this.titleRdBtn.Checked;
+            bool genreBut = this.genreRdBtn.Checked;
+            string searchData = this.searchTxtBox.Text;
 
+            listView1.Items.Clear();
+            unitTest1 = QueryDB(searchData, titleBut, genreBut, listView1);
+            Console.WriteLine(searchData);
         }
 
         private void searchTxtBox_TextChanged(object sender, EventArgs e)
--- Form3.cs
+++ Form3.cs
@@ -20,7 +20,7 @@ namespace CatalogApp
         }
 
         //Database Connection String to be accessed  whenever manipulating the DB
-        public static string connectionString = "server=10.3.129.141;database=catalog;user id=lindahlish;password=Password01;";
+        public static string connectionString = "server=192.168.183.11;database=catalog;user id=lindahlish;password=Password01;";
         public static MySqlConnection conn = new MySqlConnection(connectionString);
         public static MySqlDataReader reader;
 
